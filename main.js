1d31567d5a764a56f54f32b30a94821b9300478d
Jacob Lewallen <jlewallen@gmail.com>
2018-06-29 12:05:11 -0700

    Dumb server to test upload speeds with.

---
+++ main.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+  if (req.method == 'POST') {
+    console.log("POST", req.headers['content-type'], req.headers['content-length']);
+
+    let accepted = 0;
+    req.on('data', function(data) {
+      accepted += data.length;
+    });
+
+    req.on('end', function() {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('Ok');
+
+      console.log("DONE", accepted);
+    });
+  }
+  else {
+    console.log("GET");
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Ok');
+  }  
+}).listen(8080, "0.0.0.0");
+
+console.log('Server running at http://0.0.0.0:8080/');
